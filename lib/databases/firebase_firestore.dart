084882046d67e8fffd3151379a930ed16dceddfa
BASI <zolarpixel3@gmail.com>
2022-03-20 18:23:03 +0700

    make firebase firestore database

---
+++ lib/databases/firebase_firestore.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Room {
+  String id;
+  final Map<String, dynamic> info;
+  final List<Map<dynamic, dynamic>> users;
+
+  Room({this.id = "", required this.info, required this.users});
+
+  Map<String, dynamic> toJson() {
+    Map<String, Map<String, dynamic>> usersToMap = {};
+    Map<String, dynamic> output = {};
+
+    for (int i = 0; i < users.length; i++) {
+      String name = '';
+      Map<String, dynamic> sign = {};
+
+      users[i].forEach((key, value) {
+        if (key == "name") {
+          name = value;
+        } else {
+          sign[key] = value;
+        }
+      });
+
+      usersToMap[name] = sign;
+    }
+
+    output["info"] = info;
+    output["id"] = id;
+
+    usersToMap.forEach((key, value) {
+      output[key] = value;
+    });
+
+    debugPrint(output.toString());
+
+    return output;
+  }
+
+  Future createRoom() async {
+    final docRoom = FirebaseFirestore.instance.collection("rooms").doc();
+
+    id = docRoom.id;
+
+    final room = toJson();
+
+    await docRoom.set(room);
+  }
+}
