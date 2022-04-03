8253b9de537cd13168c0ec85e7f731cbc86a790c
BASI <zolarpixel3@gmail.com>
2022-04-04 02:55:00 +0700

    find similarity between metrics

--- lib/src/join_room_control_pages/join_room_attendance.dart
+++ lib/src/join_room_control_pages/join_room_attendance.dart
@@ -1,3 +1,6 @@
+// dart package
+import 'dart:io';
+
 // flutter
 import 'package:flutter/material.dart';
 
@@ -5,8 +8,13 @@ import 'package:flutter/material.dart';
 import 'package:atdel/src/model/room.dart';
 import 'package:atdel/src/model/attendance.dart';
 
+// image picker
+import 'package:image_picker/image_picker.dart';
+
 // services
 import 'package:atdel/src/services/room_services.dart';
+import 'package:atdel/src/services/ml_services.dart';
+import 'package:atdel/src/services/user_photo_metrics_services.dart';
 
 class JoinRoomAttendance extends StatefulWidget {
   const JoinRoomAttendance(
@@ -22,16 +30,73 @@ class JoinRoomAttendance extends StatefulWidget {
 
 class _JoinRoomAttendanceState extends State<JoinRoomAttendance> {
   final RoomService _roomService = RoomService();
+  final ImagePicker _imagePicker = ImagePicker();
+  final MLService _mlService = MLService();
+  final UserPhotoMetricService _userPhotoMetricService =
+      UserPhotoMetricService();
+
+  // get image by gallery
+  Widget pickImageByGallery() {
+    return ListTile(
+        leading: const Icon(Icons.photo),
+        title: const Text("Attend With Image Gallery"),
+        onTap: () async {
+          final XFile? file =
+              await _imagePicker.pickImage(source: ImageSource.gallery);
+
+          if (file == null) return;
+
+          final detectFace = await _mlService.runDetector(File(file.path));
+
+          final runModelMetric = await _mlService.runModel(detectFace);
+
+          await _userPhotoMetricService.updateUserPhotoMetric(
+              widget.room, runModelMetric);
+
+          await _userPhotoMetricService.calcUserSimilarity(
+              widget.room, runModelMetric);
+        });
+  }
+
+  // get image by camera
+  Widget pickImageByCamera() {
+    return ListTile(
+        leading: const Icon(Icons.camera),
+        title: const Text("Attend With Camera"),
+        onTap: () async {
+          final XFile? file =
+              await _imagePicker.pickImage(source: ImageSource.camera);
+
+          if (file == null) return;
+
+          final detectFace = await _mlService.runDetector(File(file.path));
+
+          final runModelMetric = await _mlService.runModel(detectFace);
+
+          await _userPhotoMetricService.updateUserPhotoMetric(
+              widget.room, runModelMetric);
+
+          await _userPhotoMetricService.calcUserSimilarity(
+              widget.room, runModelMetric);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Join Room Attendance")),
-        body: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  _roomService.updateAbsentUser(widget.room, widget.attendance);
-                },
-                child: const Text("Attend"))));
+      appBar: AppBar(title: const Text("Join Room Attendance")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [pickImageByGallery(), pickImageByCamera()],
+        ),
+      ),
+      // Center(
+      //     child: ElevatedButton(
+      //         onPressed: () {
+      //           _roomService.updateAbsentUser(widget.room, widget.attendance);
+      //         },
+      //         child: const Text("Attend")))
+    );
   }
 }
